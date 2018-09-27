40dccf920f610a25f78b49f52a875777c1058809
carlos <krlitozcrz@gmail.com>
2018-09-27 00:30:51 -0600

    se agrego listaro orden de trabajo

--- CrearOrdenTrabajo.php
+++ CrearOrdenTrabajo.php
@@ -86,6 +86,7 @@
                                 <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Gestión de OT<span class="caret"></span></a>
                                     <ul class="dropdown-menu" role="menu">
                                         <li><a href="#">Crear Orden de Trabajo</a></li>
+                                        <li><a href="ListarOrdenTrabajo.php">Listar Orden de Trabajo</a></li>
                                     </ul>
                                 </li>
                                 <?php

28d4f75f3945957c41d6f1dbeb175f1f8d9e069c
Gemis Guevara <gemisguevarav@gmail.com>
2018-10-04 02:55:36 -0600

    Se agrega la funcionalidad de gestionar la OT.

--- js/Modal.js
+++ js/Modal.js
@@ -1,80 +1,84 @@
 // Eliminación de usuario
- $(document).ready(function(){
-    $(document).on('click', '.EliminarUsuario', function(){
-        var id=$(this).val();
-        var Nombres=$('#NombreUsuario'+id).text();
-        var Apellidos=$('#ApellidoUsuario'+id).text();
-        var Usuario=$('#idPersonaEliminar'+id).text();
+$(document).ready(function () {
+    $(document).on('click', '.CambiarRol', function () {
+        var id = $(this).val();
+        var Nombres = $('#NombrePersona' + id).text() + " " + $('#ApellidoUsuario' + id).text() ;
 
-        $('#frmEliminar').modal('show');
-        document.querySelector('#NombresApellidos').innerText = Nombres + " " + Apellidos;
-        $('#idAEliminar').val(Usuario);
-    });
- });
-
-// Edición usuario
-$(document).ready(function(){
-    $(document).on('click', '.EditarUsuario', function(){
-        var id=$(this).val();
-        var PersonaEliminar=$('#idPersonaEliminar'+id).text();
-        var NombreUsuario=$('#NombreUsuario'+id).text();
-        var ApellidoUsuario=$('#ApellidoUsuario'+id).text();
-        var DireccionUsuario=$('#DireccionUsuario'+id).text();
-        var DPIUsuario=$('#DPIUsuario'+id).text();
-        var TelefonoUsuario=$('#TelefonoUsuario'+id).text();
-        var FechaNacUsuario=$('#FechaNacUsuario'+id).text();
-        var CorreoUsuario=$('#CorreoUsuario'+id).text();
-        var PrivilegioUsuario=$('#PrivilegioUsuario'+id).text();
-
-        $('#frmEditar').modal('show');
-        $('#idEditar').val(PersonaEliminar);
-        $('#NombreEditar').val(NombreUsuario);
-        $('#ApellidoEditar').val(ApellidoUsuario);
-        $('#DireccionEditar').val(DireccionUsuario);
-        $('#DPIEditar').val(DPIUsuario);
-        $('#TelefonoEditar').val(TelefonoUsuario);
-        $('#FechaNacEditar').val(FechaNacUsuario);
-        $('#CorreoEditar').val(CorreoUsuario);
-        $('#PrivilegioEditar').val(PrivilegioUsuario);
+        $('#ModalCambioRol').modal('show');
+        document.querySelector('#NombresApellidos').innerText = Nombres;
+        $('#idUsuarioCambioRol').val(id);
     });
 });
 
 // Deshabilitacion de Material
- $(document).ready(function(){
-    $(document).on('click', '.DeshabilitarMaterial', function(){
-        var id=$(this).val();
-        var Nombre=$('#NombreMaterial'+id).text();
-        var Producto=$('#idMaterial'+id).text();
+$(document).ready(function () {
+    $(document).on('click', '.DeshabilitarMaterial', function () {
+        var id = $(this).val();
+        var Nombre = $('#NombreMaterial' + id).text();
+        var Producto = $('#idMaterial' + id).text();
 
         $('#ModalDeshabilitar').modal('show');
         document.querySelector('#NombreMaterialDeshabilitar').innerText = Nombre;
         $('#idMaterialDeshabilitar').val(id);
     });
- });
- 
- // Habilitacion de Material
- $(document).ready(function(){
-    $(document).on('click', '.HabilitarMaterial', function(){
-        var id=$(this).val();
-        var Nombre=$('#NombreMaterial'+id).text();
-        var Producto=$('#idMaterial'+id).text();
+});
+
+// Aprobar avería
+$(document).ready(function () {
+    $(document).on('click', '.AprobarAveria', function () {
+        var id = $(this).val();
+
+        $('#ModalAprobarAveria').modal('show');
+        $('#idAprobar').val(id);
+    });
+});
+
+// Rechazar avería
+$(document).ready(function () {
+    $(document).on('click', '.RechazarAvería', function () {
+        var id = $(this).val();
+
+        $('#ModalRechazarAveria').modal('show');
+        $('#idRechazar').val(id);
+    });
+});
+
+// Habilitacion de Material
+$(document).ready(function () {
+    $(document).on('click', '.HabilitarMaterial', function () {
+        var id = $(this).val();
+        var Nombre = $('#NombreMaterial' + id).text();
+        var Producto = $('#idMaterial' + id).text();
 
         $('#ModalHabilitar').modal('show');
         document.querySelector('#NombreMaterialHabilitar').innerText = Nombre;
         $('#idMaterialHabilitar').val(id);
     });
- });
- 
- // Edición usuario
-$(document).ready(function(){
-    $(document).on('click', '.EditarPersona', function(){
-        var id=$(this).val();
-        var Persona=$('#idPersona'+id).text();
-        var NombrePersona=$('#NombrePersona'+id).text();
-        var ApellidoPersona=$('#ApellidoPersona'+id).text();
-        var DireccionPersona=$('#DireccionPersona'+id).text();
-        var TelefonoPersona=$('#TelefonoPersona'+id).text();
-        var Puesto=$('#Puesto'+id).text();
+});
+
+// Cancelar OT
+$(document).ready(function () {
+    $(document).on('click', '.CancelarOT', function () {
+        var id = $(this).val();
+        var idAveria = $('#CodigoAveria' + id).text();
+
+        $('#ModalCancelarOT').modal('show');
+        $('#idCancelar').val(id);
+        $('#idAveriaCancelar').val(idAveria);
+    });
+});
+
+// Edición de empleado
+$(document).ready(function () {
+    $(document).on('click', '.EditarPersona', function () {
+        var id = $(this).val();
+        var Persona = $('#idPersona' + id).text();
+        var NombrePersona = $('#NombrePersona' + id).text();
+        var ApellidoPersona = $('#ApellidoPersona' + id).text();
+        var DireccionPersona = $('#DireccionPersona' + id).text();
+        var TelefonoPersona = $('#TelefonoPersona' + id).text();
+        var PrecioPorHora = $('#PrecioPorHora' + id).text();
+        var Puesto = $('#Puesto' + id);
 
         $('#ModalEditarEmpleado').modal('show');
         $('#idEditar').val(Persona);
@@ -82,83 +86,84 @@ $(document).ready(function(){
         $('#ApellidoPersona').val(ApellidoPersona);
         $('#DireccionPersona').val(DireccionPersona);
         $('#TelefonoPersona').val(TelefonoPersona);
-        //$('#idTipoEmpleado').val(Puesto);
+        $('#PrecioPorHora').val(PrecioPorHora);
+        //$('#NombreTipoEmpleado').index(Puesto);
     });
 });
 
-// Deshabilitacion de Material
- $(document).ready(function(){
-    $(document).on('click', '.DeshabilitarPersona', function(){
-        var id=$(this).val();
-        var Nombre=$('#NombrePersona'+id).text();
-        var Producto=$('#idPersona'+id).text();
+// Deshabilitacion de empleado
+$(document).ready(function () {
+    $(document).on('click', '.DeshabilitarPersona', function () {
+        var id = $(this).val();
+        var Nombre = $('#NombrePersona' + id).text();
+        var Producto = $('#idPersona' + id).text();
 
         $('#ModalDeshabilitarEmpleado').modal('show');
         document.querySelector('#NombreEmpledoDeshabilitar').innerText = Nombre;
         $('#idEmpleadoDeshabilitar').val(id);
     });
- });
- 
- // Habilitacion de Material
- $(document).ready(function(){
-    $(document).on('click', '.HabilitarPersona', function(){
-        var id=$(this).val();
-        var Nombre=$('#NombrePersona'+id).text();
-        var Producto=$('#idPersona'+id).text();
+});
+
+// Habilitacion de Material
+$(document).ready(function () {
+    $(document).on('click', '.HabilitarPersona', function () {
+        var id = $(this).val();
+        var Nombre = $('#NombrePersona' + id).text();
+        var Producto = $('#idPersona' + id).text();
 
         $('#ModalHabilitarEmpleado').modal('show');
         document.querySelector('#NombreEmpleadoHabilitar').innerText = Nombre;
         $('#idEmpleadoHabilitar').val(id);
     });
- });
- 
- // Deshabilitacion de Equipo
- $(document).ready(function(){
-    $(document).on('click', '.DeshabilitarEquipo', function(){
-        var id=$(this).val();
-        var Nombre=$('#NombreEquipo'+id).text();
-        var Producto=$('#idEquipo'+id).text();
+});
+
+// Deshabilitacion de Equipo
+$(document).ready(function () {
+    $(document).on('click', '.DeshabilitarEquipo', function () {
+        var id = $(this).val();
+        var Nombre = $('#NombreEquipo' + id).text();
+        var Producto = $('#idEquipo' + id).text();
 
         $('#ModalDeshabilitarEmpleado').modal('show');
         document.querySelector('#NombreEquipoDeshabilitar').innerText = Nombre;
         $('#idEquipoDeshabilitar').val(id);
     });
- });
- 
- // Habilitacion de Equipo
- $(document).ready(function(){
-    $(document).on('click', '.HabilitarEquipo', function(){
-        var id=$(this).val();
-        var Nombre=$('#NombreEquipo'+id).text();
-        var Producto=$('#idEquipo'+id).text();
+});
+
+// Habilitacion de Equipo
+$(document).ready(function () {
+    $(document).on('click', '.HabilitarEquipo', function () {
+        var id = $(this).val();
+        var Nombre = $('#NombreEquipo' + id).text();
+        var Producto = $('#idEquipo' + id).text();
 
         $('#ModalHabilitar').modal('show');
         document.querySelector('#NombreEquipoHabilitar').innerText = Nombre;
         $('#idEquipoHabilitar').val(id);
     });
- });
- 
- // Cancelación de Avería
- $(document).ready(function(){
-    $(document).on('click', '.CancelarAveria', function(){
-        var id=$(this).val();
-        var Nombres=$('#NombreUsuario'+id).text();
-        var Apellidos=$('#ApellidoUsuario'+id).text();
-        var Usuario=$('#idPersonaEliminar'+id).text();
+});
+
+// Cancelación de Avería
+$(document).ready(function () {
+    $(document).on('click', '.CancelarAveria', function () {
+        var id = $(this).val();
+        var Nombres = $('#NombreUsuario' + id).text();
+        var Apellidos = $('#ApellidoUsuario' + id).text();
+        var Usuario = $('#idPersonaEliminar' + id).text();
 
         $('#ModalCancelarAveria').modal('show');
         //document.querySelector('#NombresApellidos').innerText = Nombres + " " + Apellidos;
         $('#idAEliminar').val(id);
     });
- });
- 
- // Edición Material
-$(document).ready(function(){
-    $(document).on('click', '.EditarMaterial', function(){
-        var id=$(this).val();
-        var NombreMaterial=$('#NombreMaterial'+id).text();
+});
+
+// Edición Material
+$(document).ready(function () {
+    $(document).on('click', '.EditarMaterial', function () {
+        var id = $(this).val();
+        var NombreMaterial = $('#NombreMaterial' + id).text();
         //var idMarca=$('#idMarca'+id).text();
-        var PrecioMaterial=$('#PrecioMaterial'+id).text();
+        var PrecioMaterial = $('#PrecioMaterial' + id).text();
 
         $('#ModalEditarMaterial').modal('show');
         $('#idEditar').val(id);
@@ -169,12 +174,12 @@ $(document).ready(function(){
 });
 
 // Edición Equipo
-$(document).ready(function(){
-    $(document).on('click', '.EditarEquipo', function(){
-        var id=$(this).val();
-        var CodigoEquipo=$('#CodigoEquipo'+id).text();
-        var NombreEquipo=$('#NombreEquipo'+id).text();
-        var PrecioMaterial=$('#CostoPorHora'+id).text();
+$(document).ready(function () {
+    $(document).on('click', '.EditarEquipo', function () {
+        var id = $(this).val();
+        var CodigoEquipo = $('#CodigoEquipo' + id).text();
+        var NombreEquipo = $('#NombreEquipo' + id).text();
+        var PrecioMaterial = $('#CostoPorHora' + id).text();
 
         $('#ModalEditarEquipo').modal('show');
         $('#idEditar').val(id);
@@ -190,7 +195,7 @@ $(document).ready(function () {
             var rex = new RegExp($(this).val(), 'i');
             $('.buscar tr').hide();
             $('.buscar tr').filter(function () {
-                    return rex.test($(this).text());
+                return rex.test($(this).text());
             }).show();
         })
     }(jQuery));
